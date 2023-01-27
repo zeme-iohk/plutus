440443f501114ea4f8c7867399d3592ec5d7b08f
zeme <zem1@pm.me>
2023-01-27 20:00:03 +0700

    WIP

--- scripts/ci-plutus-benchmark.sh
+++ scripts/ci-plutus-benchmark.sh
@@ -42,23 +42,23 @@ echo "[ci-plutus-benchmark]: Running as user:"
 whoami 
 
 echo "[ci-plutus-benchmark]: Updating cabal database ..."
-cabal update
+# cabal update
 
 echo "[ci-plutus-benchmark]: Clearing caches with cabal clean ..."
-cabal clean
+# cabal clean
 
 echo "[ci-plutus-benchmark]: Running benchmark for PR branch at $PR_BRANCH_REF ..."
-cabal bench $BENCHMARK_NAME >bench-PR.log 2>&1
+# cabal bench $BENCHMARK_NAME >bench-PR.log 2>&1
 
 echo "[ci-plutus-benchmark]: Switching branches ..."
 git checkout master
 BASE_BRANCH_REF=$(git rev-parse --short HEAD)
 
 echo "[ci-plutus-benchmark]: Clearing caches with cabal clean ..."
-cabal clean
+# cabal clean
 
 echo "[ci-plutus-benchmark]: Running benchmark for base branch at $BASE_BRANCH_REF ..."
-cabal bench $BENCHMARK_NAME >bench-base.log 2>&1
+# cabal bench $BENCHMARK_NAME >bench-base.log 2>&1
 git checkout "$PR_BRANCH_REF"  # .. so we use the most recent version of the comparison script
 exit 1
 echo "[ci-plutus-benchmark]: Comparing results ..."
